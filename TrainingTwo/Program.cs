16b5b339d09c1999ec3d51c70b6e02158d4e9e5a
Mateusz Marks <mmarks@pl.sii.eu>
2019-02-24 11:33:59 +0100

    Add units project

--- TrainingTwo/Program.cs
+++ TrainingTwo/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Units;
 
 namespace TrainingTwo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Orc orc = new Orc();
+
         }
     }
 }
